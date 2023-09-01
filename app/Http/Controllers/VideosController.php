e1064d9b1c7552cd34fbf7965f2652b1456d72f6
Arthur Villar <arthur@iMac.local>
2023-09-01 10:24:29 -0400

    Quick update

--- app/Http/Controllers/VideosController.php
+++ app/Http/Controllers/VideosController.php
@@ -17,6 +17,8 @@ class VideosController extends Controller
 
     public function upload(Request $request)
     {
+        return response(200);
+        
         $request->validate([
             'video' => 'required|mimes:mp4,mov,avi,webm,wmv',
             'email' => 'required|email',
