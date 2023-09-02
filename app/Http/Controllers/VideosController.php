518b32b461ef94ed12a7c9deb39701359b3662a8
Arthur Villar <arthur@iMac.local>
2023-09-02 13:48:13 -0400

    Quick update

--- app/Http/Controllers/VideosController.php
+++ app/Http/Controllers/VideosController.php
@@ -21,13 +21,13 @@ class VideosController extends Controller
         //     if ($request->pin != env('FILEMANAGER_SECRET'))
         //         abort(404);
         // }
-return $request->all();
+
         $request->validate([
-            'video' => 'required|mimes:mp4,mov,avi,webm,wmv',
+            'video' => 'required',
             'email' => 'required|email',
             'id' => 'required|integer'
         ]);
-
+dd('here');
         ProcessVideo::dispatch(
             Video::temporary($request->file('video'), $request->toArray())
         );
