6e68853f811c0ee1071a4a346650738ee8103ae3
Arthur Villar <arthur@iMac.local>
2023-09-03 12:13:13 -0400

    Quick update

--- resources/views/record/create.blade.php
+++ resources/views/record/create.blade.php
@@ -1,7 +1,7 @@
 <form method="POST" action="{{route('upload')}}" enctype="multipart/form-data">
     @csrf
 
-    <input type="hidden" name="secret" value="{{auth()->user()->tokens->first()->name}}">
+    <input type="hidden" name="secret" value="{{auth()->user()->tokens()->exists() ? auth()->user()->tokens->first()->name : null}}">
     <input type="hidden" name="email" value="test@email.com">
     <input type="hidden" name="id" value="1">
 
