6a3c691decad0bc2b14960e454bde5c408ef8807
Arthur Villar <arthur@iMac.local>
2023-09-07 20:24:45 -0400

    Quick update

--- resources/views/record/overlay.blade.php
+++ resources/views/record/overlay.blade.php
@@ -4,7 +4,10 @@
             <div class="progress mb-3" style="height: 25px">
                 <div class="progress-bar progress-bar-striped progress-bar-animated fw-bold" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 0%; height: 100%"></div>
             </div>
-            <div class="text-center"><strong>Please keep this browser tab open until uploading completes.</strong></div>
+            <div class="text-center">
+                <div><strong>This may take a little while!</strong></div>
+                <div>Please keep this browser tab open until uploading completes.</div>
+            </div>
         </div>
     </div>
 </div>
